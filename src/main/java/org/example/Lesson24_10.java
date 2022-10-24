03ef532a46762df33d3273a9ac8467f5be290799
Alina Jakuhina <alina.jurjane@gmail.com>
2022-10-24 14:49:31 +0200

    cikl FOR

---
+++ src/main/java/org/example/Lesson24_10.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Lesson24_10 {
+    public static void main(String[] args) {
+
+        int[] a = {87, 12, 4, 96, 42};
+
+
+
+        for (int i = 5, k = 11; i != k; i++ , k-- ) {
+            System.out.println(i + " " + k);
+
+        //int[] a = {87, 12, 4, 96, 42};
+         //        0   1   2   3   4   <- indexes
+
+      //  for (int i = 0; i < a.length; i++){
+        //    System.out.println(a[i]);
+
+        //    int valueOfLetters = 10;
+
+      //  for (int i = 0; i < valueOfLetters ; i++) {
+        //    System.out.println(i + "проверка на вес");
+        //    System.out.println(i + "проверка на наличие марки");
+        //    System.out.println(i + "проверка на размер");
+        //    System.out.println();
+        }
+    }
+}
